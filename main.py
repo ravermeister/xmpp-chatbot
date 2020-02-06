a3be0dcbcea8ad59b10f2b058fb3c119101e187d
Jonny Rimkus <jonny@rimkus.it>
2020-02-06 10:02:36 +0000

    Update main.py

--- main.py
+++ main.py
@@ -110,12 +110,11 @@ class QueryBot(slixmpp.ClientXMPP):
 					queries['xep_0072'] = await self['xep_0092'].get_version(jid=target)
 					queries['xep_0157'] = await self['xep_0030'].get_info(jid=target, cached=False)
 
-                logging.info("after query >>>>>"+target+"<<<<<")
 			except XMPPError as error:
 				logging.info(misc.HandleError(error, keyword, target).report())
 				data['reply'].append(misc.HandleError(error, keyword, target).report())
 				continue
-	
+
 			data["reply"].append(self.functions[keyword].format(queries=queries, target=target, opt_arg=opt_arg))
 
 		# remove None type from list and send all elements
