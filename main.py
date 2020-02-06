66cf2d03d7a26189227d2c1bfa21d2f408716474
Jonny Rimkus <jonny@rimkus.it>
2020-02-06 09:59:16 +0000

    Update main.py

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ class QueryBot(slixmpp.ClientXMPP):
 					queries['xep_0072'] = await self['xep_0092'].get_version(jid=target)
 					queries['xep_0157'] = await self['xep_0030'].get_info(jid=target, cached=False)
 					
-			    logging.info("after query >>>>>"+target+"<<<<<")	
+				logging.info("after query >>>>>"+target+"<<<<<")	
 			except XMPPError as error:
 				logging.info(misc.HandleError(error, keyword, target).report())
 				data['reply'].append(misc.HandleError(error, keyword, target).report())
