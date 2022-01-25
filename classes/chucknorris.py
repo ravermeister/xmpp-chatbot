7bb2ada494f3c569a99d7800692411908ad3a410
Jonny Rimkus <jonny@rimkus.it>
2022-01-25 11:14:57 +0100

    move helper class inside chucknorris class

--- classes/chucknorris.py
+++ classes/chucknorris.py
@@ -7,30 +7,6 @@ import requests
 from random import randint
 from html.parser import HTMLParser
 
-class ChuckNorrisParser(HTMLParser):
-
-    def initData(self):
-        self.jokes = []
-        self.resetTagSearch()
-        
-    def resetTagSearch(self):
-        self.article = False
-        self.paragraph = False
-        
-    def handle_starttag(self, tag, attrs):
-        if(not self.article):
-            self.article = (tag == "article")
-        elif(self.article and not self.paragraph):
-            self.paragraph = (tag == "p")
-
-    def handle_data(self, data):
-        if(self.article and self.paragraph):
-            self.jokes.append(data)
-            self.resetTagSearch()
-
-    def get_jokes(self):
-        return self.jokes
-
 class ChuckNorrisRequest:
 	"""
 	> retrieve a ChuckNorris Joke
@@ -41,23 +17,23 @@ class ChuckNorrisRequest:
 		self.api_en_1 = "https://api.icndb.com/jokes/random"
 		self.api_en_2 = "https://api.chucknorris.io/jokes/random"
 		self.api_de_1 = "https://chuck-norris-witze.de"
-	
+
 	def reply_en_2():
 		try:
 			apiUrl = self.api_en_2
 			responseJson = requests.get(apiUrl).json()
 			return responseJson["value"]
 		except Exception as error:
-			return "Error Calling Chuck Norris API: %s" % error	
-	
+			return "Error Calling Chuck Norris API: %s" % error
+
 	def reply_en_1():
 		try:
 			apiUrl = self.api_en_1
 			responseJson = requests.get(apiUrl).json()
 			return responseJson["value"]["joke"]
 		except Exception as error:
-			return "Error Calling Chuck Norris API: %s" % error	
-	
+			return "Error Calling Chuck Norris API: %s" % error
+
 	def reply_de_1():
 		try:
 			urllib3.disable_warnings()
@@ -74,7 +50,7 @@ class ChuckNorrisRequest:
 			return parser.get_jokes()[jokeIndex]
 		except Exception as error:
 			return "Fehler beim Aufruf der Chuck Norris API: %s" % error
-	
+
 	def format(self, queries, target, opt_arg):
 		self.target = target
 		self.opt_arg = opt_arg
@@ -82,3 +58,29 @@ class ChuckNorrisRequest:
 			return self.reply_de_1()
 		else:
 			return self.reply_en_1()
+	
+	
+	
+	class ChuckNorrisParser(HTMLParser):
+
+		def initData(self):
+		    self.jokes = []
+		    self.resetTagSearch()
+
+		def resetTagSearch(self):
+		    self.article = False
+		    self.paragraph = False
+
+		def handle_starttag(self, tag, attrs):
+		    if(not self.article):
+		        self.article = (tag == "article")
+		    elif(self.article and not self.paragraph):
+		        self.paragraph = (tag == "p")
+
+		def handle_data(self, data):
+		    if(self.article and self.paragraph):
+		        self.jokes.append(data)
+		        self.resetTagSearch()
+
+		def get_jokes(self):
+		    return self.jokes
